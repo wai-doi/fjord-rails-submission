8ca0aec5543bb970a149e6ab7a1ab8ee9123ed00
wai-doi <yusuke.doi@esm.co.jp>
2019-05-31 16:17:27 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_065244) do
+ActiveRecord::Schema.define(version: 2019_05_31_071050) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2019_05_31_065244) do
     t.datetime "updated_at", null: false
     t.string "author"
     t.string "picture"
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
