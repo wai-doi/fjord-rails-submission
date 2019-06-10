9a707c98ce24ba2c2b7cc7c989f8d294c371c71e
wai-doi <yusuke.doi@esm.co.jp>
2019-06-10 15:35:28 +0900

    BookとCommentをポリモーフィックで関連付けた

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,47 @@
 class CommentsController < ApplicationController
   def create
-    @report = Report.find(params[:report_id])
-    @report.comments.create(comment_params)
-    redirect_to report_path(@report)
+    if params[:book_id]
+      create_book_comment
+    elsif params[:report_id]
+      create_report_comment
+    end
   end
   
   def destroy
+    if params[:book_id]
+      destroy_book_comment
+    elsif params[:report_id]
+      destroy_report_comment
+    end
+  end
+
+  private
+
+  def create_book_comment
+    @book = Book.find(params[:book_id])
+    @book.comments.create(comment_params)
+    redirect_to book_path(@book)
+  end
+
+  def create_report_comment
+    @report = Book.find(params[:report_id])
+    @report.comments.create(comment_params)
+    redirect_to report_path(@report)
+  end
+
+  def destroy_book_comment
+    @book = Book.find(params[:book_id])
+    @comment = @book.comments.find(params[:id])
+    @comment.destroy
+    redirect_to book_path(@book)
+  end
+
+  def destroy_report_comment
     @report = Report.find(params[:report_id])
     @comment = @report.comments.find(params[:id])
     @comment.destroy
     redirect_to report_path(@report)
   end
-
-  private
 
   def comment_params
     params.require(:comment).permit(:body)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :reports do
     resources :comments, only: %i(create destroy)
   end
-  resources :books
+
+  resources :books do
+    resources :comments, only: %i(create destroy)
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
