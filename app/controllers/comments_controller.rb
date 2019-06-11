64c4bbedf0ee64dabfb97b0ada3400a20379f066
wai-doi <yusuke.doi@esm.co.jp>
2019-06-11 10:32:46 +0900

    bootcampの方法でポリモーフィックを実装した

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,49 +1,30 @@
 class CommentsController < ApplicationController
   def create
-    if params[:book_id]
-      create_book_comment
-    elsif params[:report_id]
-      create_report_comment
+    comment = Comment.new(comment_params)
+    comment.commentable = commentable
+    if comment.save
+      redirect_to comment.commentable, notice: "コメントを投稿しました。"
     end
   end
   
   def destroy
-    if params[:book_id]
-      destroy_book_comment
-    elsif params[:report_id]
-      destroy_report_comment
-    end
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to comment.commentable, notice: "コメントを削除しました。"
   end
 
   private
 
-  def create_book_comment
-    @book = Book.find(params[:book_id])
-    @book.comments.create(comment_params)
-    redirect_to book_path(@book)
-  end
-
-  def create_report_comment
-    @report = Book.find(params[:report_id])
-    @report.comments.create(comment_params)
-    redirect_to report_path(@report)
-  end
-
-  def destroy_book_comment
-    @book = Book.find(params[:book_id])
-    @comment = @book.comments.find(params[:id])
-    @comment.destroy
-    redirect_to book_path(@book)
-  end
-
-  def destroy_report_comment
-    @report = Report.find(params[:report_id])
-    @comment = @report.comments.find(params[:id])
-    @comment.destroy
-    redirect_to report_path(@report)
-  end
-
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(
+      :body,
+      :commentable_id,
+      :commentable_type
+    )
+  end
+
+  def commentable
+    klass = params[:comment][:commentable_type].constantize
+    klass.find(params[:comment][:commentable_id])
   end
 end
