328bfcd215e375c2dacacc6636d1b61b82f1cc5d
wai-doi <yusuke.doi@esm.co.jp>
2019-06-15 20:17:15 +0900

    GitHub認証ができるように

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'reports#index'
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    # get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    # get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   resources :users, only: %i(index show update)
   resources :reports do
     resources :comments, only: %i(create destroy), module: :reports
